df4e3b3632ca980a3fec9b4be2171c794a055681
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2024-08-23 19:31:32 +0300

    feat: add helmet with express wrapper

--- src/lib/api-router.ts
+++ src/lib/api-router.ts
@@ -1,11 +1,19 @@
-import { queryParser, jsonParser, ApiRouter, TNextContext } from "nexpresst";
+import {
+  queryParser,
+  jsonParser,
+  ApiRouter,
+  TNextContext,
+  expressMiddlewareWrapper,
+} from "nexpresst";
 import { errorHandler } from "./middlewares/error-handler";
 import { cors } from "./middlewares/cors";
 import { NextRequest } from "next/server";
+import helmet from "helmet";
 
 export const apiRouter = (req: NextRequest, ctx: TNextContext) =>
   new ApiRouter(req, ctx)
     .onError(errorHandler)
     .use(cors)
+    .use(expressMiddlewareWrapper(helmet()))
     .use(queryParser)
     .use(jsonParser);
