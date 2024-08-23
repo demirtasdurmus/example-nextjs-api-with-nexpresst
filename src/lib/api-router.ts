78807bbb9d3466ffd2f75b17d14000fd0a14a7e9
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2024-08-23 19:37:25 +0300

    feat: add cors with express wrapper

--- src/lib/api-router.ts
+++ src/lib/api-router.ts
@@ -6,14 +6,15 @@ import {
   expressMiddlewareWrapper,
 } from "nexpresst";
 import { errorHandler } from "./middlewares/error-handler";
-import { cors } from "./middlewares/cors";
+// import { cors } from "./middlewares/cors";
 import { NextRequest } from "next/server";
 import helmet from "helmet";
+import cors from "cors";
 
 export const apiRouter = (req: NextRequest, ctx: TNextContext) =>
   new ApiRouter(req, ctx)
     .onError(errorHandler)
-    .use(cors)
+    .use(expressMiddlewareWrapper(cors({ origin: "http://localhost:3000" })))
     .use(expressMiddlewareWrapper(helmet()))
     .use(queryParser)
     .use(jsonParser);
